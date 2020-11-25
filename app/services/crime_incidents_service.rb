60b0f4608b3759306e08546998bdb0aa82878fc8
Vutivi Shivambu <vutivi.xanani@gmail.com>
2020-11-25 23:35:00 +0200

    Update crime_incidents_service.rb
    Change url

--- app/services/crime_incidents_service.rb
+++ app/services/crime_incidents_service.rb
@@ -1,6 +1,6 @@
 class CrimeIncidentsService
   def self.get_crime_incidents
-    response = HTTParty.get('https://data.code4sa.org/resource/fgg4-qjnk.json/')
+    response = HTTParty.get('https://s3-eu-west-1.amazonaws.com/openup-ckan/openup-ckan/resources/472ac8cd-cd61-4553-a32f-68019cbbf49f/8d9f-f6rq.json?AWSAccessKeyId=AKIAYIFP5EK2O3A7YP4F&Expires=1606340033&Signature=GENxFbHF3HV0WBxni91Gxs9v4Tw%3D')
     JSON.parse(response.body)
   end
 end
