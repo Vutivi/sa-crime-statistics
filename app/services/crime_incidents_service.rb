e6e590deb1237a9806a7456a2a8381ec8bea52dd
Vutivi Shivambu <vutivi.xanani@gmail.com>
2020-11-25 23:46:43 +0200

    Update crime_incidents_service.rb

--- app/services/crime_incidents_service.rb
+++ app/services/crime_incidents_service.rb
@@ -1,6 +1,6 @@
 class CrimeIncidentsService
   def self.get_crime_incidents
-    response = HTTParty.get('https://s3-eu-west-1.amazonaws.com/openup-ckan/openup-ckan/resources/472ac8cd-cd61-4553-a32f-68019cbbf49f/8d9f-f6rq.json?AWSAccessKeyId=AKIAYIFP5EK2O3A7YP4F&Expires=1606340033&Signature=GENxFbHF3HV0WBxni91Gxs9v4Tw%3D')
+    response = HTTParty.get('https://data.openup.org.za/dataset/14ab1e0b-0526-4b5d-ade9-4c00af5e1392/resource/472ac8cd-cd61-4553-a32f-68019cbbf49f/download/8d9f-f6rq.json')
     JSON.parse(response.body)
   end
 end
